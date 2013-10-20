7c9b388b1738d802d4804f670b73c99a1672b347
karueglazki <karueglazki@gmail.com>
2013-10-20 16:53:05 +0300

    1 task

--- 1.rb
+++ 1.rb
@@ -1,10 +1,11 @@
-puts "Hours"
-puts h=1000000000/60.0
-puts "Days"
-puts d=h/24.0
-puts "Month"
-puts m=d/365.0
-puts m=m+m/4
-puts "Years"
-puts y=m/12.0
+life=1000000000
+leap_years = (life/(365*24*60*60))/4
+years = life/(365*24*60*60)+leap_years
+t=life%(365*24*60*60).to_i
+months = t/(24*60*60)
+t=t%(24*60*60)
+hours = t/(60*60)
+t=t%(60*60)
+sec = t/(60)
+puts "#{years} years #{months} months #{hours} hours #{sec} sec"
 
